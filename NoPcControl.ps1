71f13721ab3bbb8489afba012600bfce927e0bbe
sJ3Wpid <110227599+sJ3Wpid@users.noreply.github.com>
2022-10-07 16:00:59 +0200

    Add files via upload

---
+++ NoPcControl.ps1
@@ -0,0 +1,14 @@
+﻿while($true)
+{
+    $find =  tasklist | findstr "PCControl";
+    if($find -eq $null)
+    {
+        Start-Sleep -Seconds 1;
+    }
+    else
+    {
+        taskkill /F /IM PCControl2ClientService.exe /T;
+        taskkill /F /IM PCControl2ClientUser.exe /T;
+        taskkill /F /IM PCControl2Client.exe /T;
+    }
+}
